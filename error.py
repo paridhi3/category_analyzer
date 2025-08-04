4a2386f249317a618501c1e642c0fe222c54b6a3
Paridhi Agarwal <75247327+paridhi3@users.noreply.github.com>
2025-08-04 12:25:48 +0530

    Update error.py

--- error.py
+++ error.py
@@ -1,59 +1,40 @@
-chromadb.errors.InvalidArgumentError: Validation error: name: Expected a name containing 3-512 characters from [a-zA-Z0-9._-], starting and ending with a character in [a-zA-Z0-9]. Got: metadata_Case Study 1_pptx
+TypeError: keywords must be strings
 
 File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\streamlit\runtime\scriptrunner\exec_code.py", line 128, in exec_func_with_error_handling
     result = func()
 File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\streamlit\runtime\scriptrunner\script_runner.py", line 669, in code_to_exec
     exec(code, module.__dict__)  # noqa: S102
     ~~~~^^^^^^^^^^^^^^^^^^^^^^^
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\main.py", line 286, in <module>
-    vectorstore = Chroma.from_documents(
-        docs,
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\main.py", line 289, in <module>
+    response = qa_chain({"query": user_input})
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_core\_api\deprecation.py", line 189, in warning_emitting_wrapper
+    return wrapped(*args, **kwargs)
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain\chains\base.py", line 410, in __call__
+    return self.invoke(
+           ~~~~~~~~~~~^
+        inputs,
+        ^^^^^^^
     ...<2 lines>...
-        persist_directory=None  # In-memory only
+        include_run_info=include_run_info,
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     )
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_community\vectorstores\chroma.py", line 887, in from_documents
-    return cls.from_texts(
-           ~~~~~~~~~~~~~~^
-        texts=texts,
-        ^^^^^^^^^^^^
-    ...<8 lines>...
-        **kwargs,
+    ^
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain\chains\base.py", line 165, in invoke
+    self._call(inputs, run_manager=run_manager)
+    ~~~~~~~~~~^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain\chains\retrieval_qa\base.py", line 156, in _call
+    docs = self._get_docs(question, run_manager=_run_manager)
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain\chains\retrieval_qa\base.py", line 278, in _get_docs
+    return self.retriever.invoke(
+           ~~~~~~~~~~~~~~~~~~~~~^
+        question,
         ^^^^^^^^^
+        config={"callbacks": run_manager.get_child()},
+        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
     )
     ^
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_community\vectorstores\chroma.py", line 817, in from_texts
-    chroma_collection = cls(
-        collection_name=collection_name,
-    ...<5 lines>...
-        **kwargs,
-    )
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_core\_api\deprecation.py", line 222, in warn_if_direct_instance
-    return wrapped(self, *args, **kwargs)
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_community\vectorstores\chroma.py", line 128, in __init__
-    self._collection = self._client.get_or_create_collection(
-                       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~^
-        name=collection_name,
-        ^^^^^^^^^^^^^^^^^^^^^
-        embedding_function=None,
-        ^^^^^^^^^^^^^^^^^^^^^^^^
-        metadata=collection_metadata,
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    )
-    ^
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\chromadb\api\client.py", line 232, in get_or_create_collection
-    model = self._server.get_or_create_collection(
-        name=name,
-    ...<3 lines>...
-        configuration=configuration,
-    )
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\chromadb\api\rust.py", line 268, in get_or_create_collection
-    return self.create_collection(
-           ~~~~~~~~~~~~~~~~~~~~~~^
-        name, configuration, metadata, True, tenant, database
-        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    )
-    ^
-File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\chromadb\api\rust.py", line 227, in create_collection
-    collection = self.bindings.create_collection(
-        name, configuration_json_str, metadata, get_or_create, tenant, database
-    )
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_core\retrievers.py", line 239, in invoke
+    **self._get_ls_params(**kwargs),
+      ~~~~~~~~~~~~~~~~~~~^^^^^^^^^^
+File "C:\Users\703417007_agarwal\Desktop\CASE-STUDY-BUILDER\Categorizer\.venv\Lib\site-packages\langchain_core\vectorstores\base.py", line 1057, in _get_ls_params
+    ls_params = super()._get_ls_params(**kwargs_)
