24c8d43b4d940c024b42818ed9073dccd0693679
Paridhi Agarwal <75247327+paridhi3@users.noreply.github.com>
2025-08-05 14:10:33 +0530

    Update llmconfig.py

--- llmconfig.py
+++ llmconfig.py
@@ -1,8 +1,10 @@
-# llm_config.py
+# config.py
 
 import os
 from dotenv import load_dotenv
 from langchain_openai import AzureChatOpenAI
+from langchain_community.document_loaders import AzureBlobStorageContainerLoader
+from langchain.embeddings import AzureOpenAIEmbeddings
 
 load_dotenv()
 
@@ -12,4 +14,16 @@ llm = AzureChatOpenAI(
     openai_api_version=os.getenv("API_VERSION"),
     deployment_name=os.getenv("MODEL_NAME"),
     temperature=0
+)
+
+loader = AzureBlobStorageContainerLoader(
+    conn_str=os.getenv("AZURE_STORAGE_CONNECTION_STRING"),
+    container=os.getenv("AZURE_STORAGE_CONTAINER_NAME")
+)
+
+embedding_model = AzureOpenAIEmbeddings(
+    azure_deployment="text-embedding-ada-002",
+    openai_api_version=os.getenv("API_VERSION"),
+    azure_endpoint="https://gen-cim-eas-dep-genai-train-openai.openai.azure.com/",
+    chunk_size=500
 )
